d359576e5e26c7b1d3d6f5bb4325021812d9a35d
kertCI <kert@car.info>
2024-08-31 16:29:49 +0800

    added IDE helper package for better intellisense

--- api/app/Models/Course.php
+++ api/app/Models/Course.php
@@ -5,6 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $course_title
+ * @property string $course_code
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\CourseFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Course newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Course newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Course query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereCourseCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereCourseTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Course extends Model
 {
     use HasFactory;
--- api/app/Models/SchoolYear.php
+++ api/app/Models/SchoolYear.php
@@ -3,9 +3,30 @@
 namespace App\Models;
 
 use Database\Factories\SchoolYearFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $school_year
+ * @property string $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\SchoolYearFactory factory($count = null, $state = [])
+ * @method static Builder|SchoolYear newModelQuery()
+ * @method static Builder|SchoolYear newQuery()
+ * @method static Builder|SchoolYear open()
+ * @method static Builder|SchoolYear query()
+ * @method static Builder|SchoolYear whereCreatedAt($value)
+ * @method static Builder|SchoolYear whereId($value)
+ * @method static Builder|SchoolYear whereSchoolYear($value)
+ * @method static Builder|SchoolYear whereStatus($value)
+ * @method static Builder|SchoolYear whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class SchoolYear extends Model
 {
     use HasFactory;
@@ -28,5 +49,10 @@ class SchoolYear extends Model
         if ($model->status === 'open') {
             SchoolYear::query()->where('id', '!=', $model->id)->update(['status' => 'close']);
         }
+    }
+
+    public function scopeOpen(Builder $query): Builder
+    {
+        return $query->where('status', 'open');
     }
 }
